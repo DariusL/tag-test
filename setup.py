5006fbf8197e707dba0bee491439b777ea7bb4b1
Darius Lapūnas <d.lapunas@gmail.com>
2017-03-24 14:36:58 +0200

    Add sample setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import glob
+import os
+import sys
+from os.path import splitext, basename
+
+try:
+    from setuptools import setup, Command, find_packages
+except ImportError:
+    from distutils.core import setup, Command, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    # noinspection PyAttributeOutsideInit
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        # Add any arguments here.
+        self.pytest_args = []
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+setup(name='foobar',
+      version='0.0.1',
+      author='Arxan Technologies, Inc.',
+      author_email='devops@arxan.com',
+      url='http://www.arxan.com',
+      description='Python TestDriver.',
+      zip_safe=False,
+      )
+
